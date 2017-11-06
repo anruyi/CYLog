730306584e2c757c13cf92e7b05a6420200e61e1
CYLeft <unrequired@163.com>
2017-11-06 22:40:22 +0800

    change some message in conf

--- core/lib/Log.php
+++ core/lib/Log.php
@@ -12,17 +12,25 @@ class Log
 {
 
     /**
-     * @var
      * 选择调用的控制方法，是file存储还是mysql存储
      */
     static $class;
 
+    /**
+     * 初始化log类
+     */
     static public function init()
     {
+        /**
+         * 从conf中选择log类所使用的驱动
+         * 这里使用的是lib/drive/log/file
+         * [注:]此处自动include conf类
+         */
         $drive = Conf::get('DRIVE','log');
         $class = '\core\lib\drive\log\\'.$drive;
         self::$class = new $class; //把弄过来的变量地址作为新的对象，神来之作。
     }
+
     static public function log($name,$file='log')
     {
         self::$class->log($name,$file);
