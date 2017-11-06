aa50c64f0c4b667995ac54dba6888d577f8a2c80
cy <unrequired@163.com>
2017-11-06 22:37:05 +0800

    add some message in it

--- index.php
+++ index.php
@@ -1,19 +1,38 @@
 <?php
 /**
- * 入口文件，index.php
+ * 入口文件,index.php
  * Date: 17-8-18
- * Time: 11:15
+ * author: CY
+ *
+ * [解释:]
+ * 一 定义路径变量,方便后续使用
+ * 二 引入基础文件,并打开自动装载类,debug类
+ * 三 启动框架
  */
+
 use core\CyPHP;
 
-//当前框架所在的根目录
+/**
+ * 当前框架所在目录
+ */
 define('CYPHP',realpath('./'));
-//核心文件core所处目录
+
+/**
+ * 核心文件core所处目录
+ */
 define('CORE',CYPHP.'/core');
-//项目文件：控制器、模型……
+
+/**
+ * 项目文件：控制器、模型……
+ */
 define('APP',CYPHP.'/app');
-//调试
+
+/**
+ * 调试开关,默认开启
+ */
 define('DEBUG',true);
+
+
 define('MODEL','/app');
 
 /**
@@ -38,11 +57,23 @@ if(DEBUG){
     ini_set('display_errors','Off');
 }
 
+/**
+ * 引入常用基本方法
+ */
 include CORE.'/common/function.php';
+
+/**
+ * 引入框架
+ */
 include CORE.'/CyPHP.php';
 
-//设置自动加载,传递过去的参数是当前缺乏的文件地址
+/**
+ * 设置自动加载,传递过去的参数是当前缺乏的文件地址
+ * (自动加载route类)
+ */
 spl_autoload_register("core\CyPHP::load");
 
-
+/**
+ * 启动框架了,开不开心,意不意外?
+ */
 CyPHP::run();
